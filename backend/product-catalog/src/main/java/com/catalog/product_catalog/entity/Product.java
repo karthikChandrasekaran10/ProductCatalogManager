ca3162f473e0dc25312054211e76e1a858d01781
Karthik Chandrasekaran <karthikchandrasekaran326@gmail.com>
2025-05-12 20:07:21 -0500

    test

--- backend/product-catalog/src/main/java/com/catalog/product_catalog/entity/Product.java
+++ backend/product-catalog/src/main/java/com/catalog/product_catalog/entity/Product.java
@@ -15,7 +15,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id2;
 
     private String name;
     private String description;
